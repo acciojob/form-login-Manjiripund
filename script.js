c0884eb207633b73d90559e95b983583ed929ff0
Manjiri Pund <113796944+Manjiripund@users.noreply.github.com>
2025-12-18 03:01:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+const fname = document.querySelector("input[type='text' , name='fname']").value;
+const lname = document.querySelector("input[type='text' ,name='lname']").value;
+
+	const btn = document.querySelector("input[type='submit']").value;
+
+	btn.addEventListener("click",() =>{
+		alert(fname+" "+lname);
+	});
+
 
 }
